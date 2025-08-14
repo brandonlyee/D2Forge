88a9aebfae872b188ec97c862b98a6633c4efc01
brandonlyee <brandonlcoyee@gmail.com>
2025-08-14 00:42:58 -0700

    Vercel analytics

--- d2-stat-tuner-frontend/src/app/layout.tsx
+++ d2-stat-tuner-frontend/src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Analytics } from "@vercel/analytics/next";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -39,6 +41,7 @@ export default function RootLayout({
         >
           {children}
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
